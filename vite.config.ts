618f569c36135b7e0cdc375982e250355b6a9fde
Anas Badran <ansaccts@gmail.com>
2025-05-20 16:58:38 +0300

    removed cursor suggestionthat does NOT work

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,5 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()],
-	css: {
-		modules: {
-			generateScopedName: (name) => `vp-${name}` // 'vp-' prefix for Viewport Playground
-		}
-	}
+	plugins: [tailwindcss(), sveltekit()]
 });
