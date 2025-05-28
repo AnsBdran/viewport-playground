2221e13016ce0678e350b92034495c587467bceb
Anas Badran <ansaccts@gmail.com>
2025-05-28 13:11:44 +0300

    added input & select styles updated

--- src/lib/ui/index.ts
+++ src/lib/ui/index.ts
@@ -2,3 +2,4 @@ export { default as Select } from './select.svelte';
 export { default as Demo } from './demo.svelte';
 export { default as Button } from './button.svelte';
 export * from './icons.svelte';
+export { default as Input } from './input.svelte';
---
+++ src/lib/ui/input.svelte
@@ -0,0 +1,18 @@
+<script>
+	let { value = $bindable(), ...rest } = $props();
+</script>
+
+<input {...rest} bind:value class="vp-input" />
+
+<style>
+	.vp-input {
+		border: 1px solid #ccc;
+		padding-inline: 0.5rem;
+		padding-block: 0.25rem;
+		border-radius: 6px;
+		outline: none;
+	}
+	.vp-input:focus {
+		border-color: #aaa;
+	}
+</style>
